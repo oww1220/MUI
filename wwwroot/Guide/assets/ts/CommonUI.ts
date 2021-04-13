d917fa21fcfe6e16629481e4e5a0456dc29ac9ff
이동걸 <oww@mz.co.kr>
2021-04-13 10:34:20 +0900

    index.d.ts adding!

--- wwwroot/Guide/assets/ts/CommonUI.ts
+++ wwwroot/Guide/assets/ts/CommonUI.ts
@@ -1,23 +1,3 @@
-
-declare const Swiper: (target: any, option: any) => void;
-declare const IScroll: (item: any, option: any) => void;
-
-interface JQuery {
-    slick(option: any): JQuery;
-    datepicker(parm1: any, parm2?: any): any;
-}
-
-interface HTMLElement {
-    iscrolls?: object;
-}
-
-interface Iiscrolls {
-    cash: any;
-    num: number;
-    init(target: string, option: any): void;
-    resize(): void;
-}
-
 namespace CommonUI {
     export const $: JQueryStatic = jQuery;
     export const resize={
